0bcc400ab043f984c046251030bb5dbdb320468b
Anton Saatze <a.saatze@googlemail.com>
2020-10-05 17:51:26 +0200

    add icons for exercises

--- app/src/main/java/com/example/furstychrismas/model/Muscle.kt
+++ app/src/main/java/com/example/furstychrismas/model/Muscle.kt
@@ -3,12 +3,12 @@ package com.example.furstychrismas.model
 import com.example.furstychrismas.R
 
 enum class Muscle(val muscle:String,val icon:Int) {
-    LEG("legs", R.drawable.body),
-    ARM("arms", R.drawable.ball),
-    CHEST("chest", R.drawable.infinite),
-    ABS("abs", R.drawable.ball),
+    LEG("legs", R.drawable.ic_legs),
+    ARM("arms", R.drawable.ic_arm),
+    CHEST("chest", R.drawable.ic_chest),
+    ABS("abs", R.drawable.ic_abs),
     NECK("neck", R.drawable.ball),
-    BACK("back", R.drawable.ball),
+    BACK("back", R.drawable.ic_back),
     STABILITY("stability", R.drawable.ball),
     ALL("all", R.drawable.ball),
     FLEXIBILITY("flexibility", R.drawable.ball);
