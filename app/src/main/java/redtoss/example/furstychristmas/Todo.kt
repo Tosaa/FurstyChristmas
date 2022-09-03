75cf780a2c1f54546dadea688281e42345993ce4
Anton Saatze <a.saatze@googlemail.com>
2022-09-03 22:38:52 +0200

    add Todos

--- app/src/main/java/redtoss/example/furstychristmas/Todo.kt
+++ app/src/main/java/redtoss/example/furstychristmas/Todo.kt
@@ -1,1 +1,18 @@
 package redtoss.example.furstychristmas
+
+/*
+Refactor Code
+Add Workouts and Infos for 2022 Calendar version
+Remove unused / deprecated classes
+Create ScreenUtil methods which can be shared (reduce Code duplication)
+Move Repositories, Persistence etc. to own module
+Improve Exercise UI
+Add Image resolving
+Create FurstyTheme and apply it on each screen
+Cleanup build.gradle.kts
+
+Cool maybe thing:
+Use Player Trikos as Calendar doors
+Add Animation when Calenar door is opened
+
+ */
